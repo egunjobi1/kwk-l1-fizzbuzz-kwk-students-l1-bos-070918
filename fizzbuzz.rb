65c8e08e8e080ef0dc33c7cb56e2762897dc2bb6
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-10 20:22:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
- 
+  elsif int % 15 == 0
+    "Fizzbuzz"
   if int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz" 
-  elsif int % 15 == 0
-    "Fizzbuzz"
+ 
     
   else nil 
     
